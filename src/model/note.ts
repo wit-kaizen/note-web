4cf47ae52f9da533852e81bb8a82aa04fbb08944
tang.tyf <tang.tyf@alibaba-inc.com>
2021-07-01 14:20:17 +0800

    fix: 类声明方式在ES6和TS之间的差异性

--- src/model/note.ts
+++ src/model/note.ts
@@ -5,26 +5,30 @@ export default class Note {
     createAt: Number
     done: Boolean
   */
-  constructor(category, content, createAt) {
-    this.category = category;
-    this.content = content;
-    this.createAt = createAt;
-    this.done = false;
+  category: string[]
+  content: string
+  createAt: number
+  done: boolean
+  constructor(category: string[], content: string, createAt: number) {
+    this.category = category
+    this.content = content
+    this.createAt = createAt
+    this.done = false
   }
-}
 
-Note.createFromText = function(input) {
-  let match = input.match(/^#(.+)#\s{1,}(\d{4}-\d{2}-\d{2})?\s?([\w\W]+)$/);
-  let text = input;
-  let createAt = null;
-  let category = 'n/a';
+  static createFromText(input: string) {
+    let match = input.match(/^#(.+)#\s{1,}(\d{4}-\d{2}-\d{2})?\s?([\w\W]+)$/)
+    let text = input
+    let createAt = null
+    let category = 'n/a'
 
-  if(match) {
-    category = match[1];
-    text = match[3]
-    createAt = match[2] ? match[2] : null;
+    if (match) {
+      category = match[1]
+      text = match[3]
+      createAt = match[2] ? match[2] : null
+    }
+    let time = createAt ? new Date(createAt).getTime() : new Date().getTime()
+
+    return new Note([category], text, time)
   }
-  let time = createAt ? (new Date(createAt)).getTime() : (new Date()).getTime();
-
-  return new Note([category], text, time);
 }
